85d760ec1b49b2c24e9dfdbe27db16481b701039
m.maheshbhagirath@gmail.com <Mahesh>
2022-08-25 12:05:35 +0530

    Json dependency issue fixed

--- org.ancit.json.ui/src/org/ancit/json/ui/dialog/Node1Dialog.java
+++ org.ancit.json.ui/src/org/ancit/json/ui/dialog/Node1Dialog.java
@@ -1,72 +1,79 @@
+/*
+ * FILE:            Node1Dialog.java
+ *
+ * SW-COMPONENT:    org.ancit.json.ui
+ *
+ * DESCRIPTION:     -
+ *
+ * COPYRIGHT:       © 2015 - 2022 Robert Bosch GmbH
+ *
+ * The reproduction, distribution and utilization of this file as
+ * well as the communication of its contents to others without express
+ * authorization is prohibited. Offenders will be held liable for the
+ * payment of damages. All rights reserved in the event of the grant
+ * of a patent, utility model or design.
+ */
 package org.ancit.json.ui.dialog;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.nebula.widgets.calendarcombo.CalendarCombo;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Shell;
-
-import Json.Node1;
-import Json.Node2;
-import Json.Type;
-
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 import com.ancit.json.ui.jet.JsonGenerator;
 
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Combo;
-import org.eclipse.jface.viewers.ComboViewer;
-import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.DateTime;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.events.MouseTrackAdapter;
-import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.nebula.widgets.calendarcombo.CalendarCombo;
-
+import Json.Node1;
+import Json.Type;
 
 public class Node1Dialog extends Dialog {
 	private static class ViewerLabelProvider extends LabelProvider {
+		@Override
 		public Image getImage(Object element) {
 			return super.getImage(element);
 		}
 
+		@Override
 		public String getText(Object element) {
 			if (element instanceof Type) {
 				Type type = (Type) element;
 				return type.getLiteral();
-			} else {
-				return null;
 			}
+			return null;
 		}
 	}
 
 	private static class ContentProvider implements IStructuredContentProvider {
+		@Override
 		public Object[] getElements(Object inputElement) {
 			return new Object[0];
 		}
 
+		@Override
 		public void dispose() {
 		}
 
+		@Override
 		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
 		}
 	}
@@ -99,28 +106,24 @@ public class Node1Dialog extends Dialog {
 		gridLayout.numColumns = 2;
 
 		Label lblNewLabel = new Label(container, SWT.NONE);
-		lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false,
-				false, 1, 1));
+		lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblNewLabel.setText("Name");
 
 		text = new Text(container, SWT.BORDER);
 		if (node1.getName() != null) {
 			text.setText(node1.getName());
 		}
-		text.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
+		text.addModifyListener(e -> {
 
-				if (text != null) {
-					node1.setName(text.getText());
-				}
-
+			if (text != null) {
+				node1.setName(text.getText());
 			}
+
 		});
 		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
 		Label lblType = new Label(container, SWT.NONE);
-		lblType.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false,
-				1, 1));
+		lblType.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblType.setText("Type");
 		List<Type> values = Type.VALUES;
 		comboViewer = new ComboViewer(container, SWT.NONE);
@@ -129,33 +132,29 @@ public class Node1Dialog extends Dialog {
 		if (node1.getType() != null) {
 			combo.setText(node1.getType().getLiteral());
 		}
-		combo.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				IStructuredSelection iStructuredSelection = (IStructuredSelection) comboViewer
-						.getSelection();
-				Type type = (Type) iStructuredSelection.getFirstElement();
+		combo.addModifyListener(e -> {
+			IStructuredSelection iStructuredSelection = (IStructuredSelection) comboViewer.getSelection();
+			Type type = (Type) iStructuredSelection.getFirstElement();
 
-				node1.setType(type);
+			node1.setType(type);
 
-			}
 		});
-		combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1,
-				1));
+		combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
 		Label lblNewLabel_1 = new Label(container, SWT.NONE);
 		lblNewLabel_1.setText("Date");
-		
+
 		calendarCombo = new CalendarCombo(container, SWT.NONE);
 		calendarCombo.addModifyListener(new ModifyListener() {
+			@Override
 			public void modifyText(ModifyEvent e) {
-				
+
 				System.out.println(calendarCombo.getDate());
-				
-				
+
 			}
 		});
 		calendarCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
-		
+
 		comboViewer.setLabelProvider(new ViewerLabelProvider());
 		comboViewer.setContentProvider(ArrayContentProvider.getInstance());
 		comboViewer.setInput(values);
@@ -169,11 +168,9 @@ public class Node1Dialog extends Dialog {
 	 */
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
-		Button button = createButton(parent, IDialogConstants.OK_ID,
-				IDialogConstants.OK_LABEL, true);
+		Button button = createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
 		button.setText("Generate");
-		createButton(parent, IDialogConstants.CANCEL_ID,
-				IDialogConstants.CANCEL_LABEL, false);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
 	}
 
 	/**
@@ -183,10 +180,11 @@ public class Node1Dialog extends Dialog {
 	protected void okPressed() {
 		// TODO Auto-generated method stub
 		super.okPressed();
-		JsonGenerator generator=new JsonGenerator();
+		JsonGenerator generator = new JsonGenerator();
 		String generate = generator.generate(node1);
 		FileExportDialog.export(generate);
 	}
+
 	@Override
 	protected Point getInitialSize() {
 		return new Point(450, 300);
--- org.ancit.json.ui/src/org/ancit/json/ui/dialog/Node2Dialog.java
+++ org.ancit.json.ui/src/org/ancit/json/ui/dialog/Node2Dialog.java
@@ -1,3 +1,18 @@
+/*
+ * FILE:            Node2Dialog.java
+ *
+ * SW-COMPONENT:    org.ancit.json.ui
+ *
+ * DESCRIPTION:     -
+ *
+ * COPYRIGHT:       © 2015 - 2022 Robert Bosch GmbH
+ *
+ * The reproduction, distribution and utilization of this file as
+ * well as the communication of its contents to others without express
+ * authorization is prohibited. Offenders will be held liable for the
+ * payment of damages. All rights reserved in the event of the grant
+ * of a patent, utility model or design.
+ */
 package org.ancit.json.ui.dialog;
 
 import java.text.DateFormat;
@@ -8,62 +23,61 @@ import java.util.List;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseTrackAdapter;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Shell;
-
-import Json.Node2;
-import Json.Type;
-
+import org.eclipse.swt.widgets.DateTime;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 import com.ancit.json.ui.jet.JsonGenerator;
 
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Combo;
-import org.eclipse.jface.viewers.ComboViewer;
-import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.DateTime;
-import org.eclipse.swt.widgets.Button;
-
-import org.eclipse.swt.events.MouseTrackAdapter;
-import org.eclipse.swt.events.MouseEvent;
+import Json.Node2;
+import Json.Type;
 
 public class Node2Dialog extends Dialog {
 	private static class ViewerLabelProvider extends LabelProvider {
+		@Override
 		public Image getImage(Object element) {
 			return super.getImage(element);
 		}
 
+		@Override
 		public String getText(Object element) {
 			if (element instanceof Type) {
 				Type type = (Type) element;
 				return type.getLiteral();
-			} else {
-				return null;
 			}
+			return null;
 		}
 	}
 
 	private static class ContentProvider implements IStructuredContentProvider {
+		@Override
 		public Object[] getElements(Object inputElement) {
 			return new Object[0];
 		}
 
+		@Override
 		public void dispose() {
 		}
 
+		@Override
 		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
 		}
 	}
@@ -96,28 +110,24 @@ public class Node2Dialog extends Dialog {
 		gridLayout.numColumns = 2;
 
 		Label lblNewLabel = new Label(container, SWT.NONE);
-		lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false,
-				false, 1, 1));
+		lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblNewLabel.setText("Name");
 
 		text = new Text(container, SWT.BORDER);
 		if (node2.getName() != null) {
 			text.setText(node2.getName());
 		}
-		text.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
+		text.addModifyListener(e -> {
 
-				if (text != null) {
-					node2.setName(text.getText());
-				}
-
+			if (text != null) {
+				node2.setName(text.getText());
 			}
+
 		});
 		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
 		Label lblType = new Label(container, SWT.NONE);
-		lblType.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false,
-				1, 1));
+		lblType.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblType.setText("Type");
 		List<Type> values = Type.VALUES;
 		comboViewer = new ComboViewer(container, SWT.NONE);
@@ -125,18 +135,14 @@ public class Node2Dialog extends Dialog {
 		if (node2.getType().getLiteral() != null) {
 			combo.setText(node2.getType().getLiteral());
 		}
-		combo.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				IStructuredSelection iStructuredSelection = (IStructuredSelection) comboViewer
-						.getSelection();
-				Type type = (Type) iStructuredSelection.getFirstElement();
+		combo.addModifyListener(e -> {
+			IStructuredSelection iStructuredSelection = (IStructuredSelection) comboViewer.getSelection();
+			Type type = (Type) iStructuredSelection.getFirstElement();
 
-				node2.setType(type);
+			node2.setType(type);
 
-			}
 		});
-		combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1,
-				1));
+		combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
 		Label lblNewLabel_1 = new Label(container, SWT.NONE);
 		lblNewLabel_1.setText("Date");
@@ -166,8 +172,7 @@ public class Node2Dialog extends Dialog {
 
 			}
 		});
-		dateTime.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false,
-				1, 1));
+		dateTime.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 
 		// DateTime dateTime = new DateTime(container, SWT.BORDER);
 
@@ -184,11 +189,9 @@ public class Node2Dialog extends Dialog {
 	 */
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
-		Button button = createButton(parent, IDialogConstants.OK_ID,
-				IDialogConstants.OK_LABEL, true);
+		Button button = createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
 		button.setText("Generate");
-		createButton(parent, IDialogConstants.CANCEL_ID,
-				IDialogConstants.CANCEL_LABEL, false);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
 	}
 
 	/**
@@ -198,10 +201,11 @@ public class Node2Dialog extends Dialog {
 	protected void okPressed() {
 		// TODO Auto-generated method stub
 		super.okPressed();
-		JsonGenerator generator=new JsonGenerator();
+		JsonGenerator generator = new JsonGenerator();
 		String generate = generator.generate(node2);
 		FileExportDialog.export(generate);
 	}
+
 	@Override
 	protected Point getInitialSize() {
 		return new Point(450, 300);
